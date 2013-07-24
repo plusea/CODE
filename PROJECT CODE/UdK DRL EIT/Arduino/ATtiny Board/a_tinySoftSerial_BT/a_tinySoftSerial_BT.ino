145a3115a90ce944490f84a9209297d117c9f02b
Plusea <plusea@pluseas-macbook-2.local>
2013-07-24 13:46:05 +0200

    adding ATtiny Bluetooth code

--- PROJECT CODE/UdK DRL EIT/Arduino/ATtiny Board/a_tinySoftSerial_BT/a_tinySoftSerial_BT.ino
+++ PROJECT CODE/UdK DRL EIT/Arduino/ATtiny Board/a_tinySoftSerial_BT/a_tinySoftSerial_BT.ino
@@ -3,11 +3,11 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(1, 0); // RX, TX
-
+int inByte;
 void setup()
 {
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+  mySerial.begin(57600);
   mySerial.println("starting...");
   pinMode(2, INPUT);
   digitalWrite(2, HIGH);
@@ -19,15 +19,32 @@ void setup()
 
 void loop() // run over and over
 {
+  if (mySerial.available())
+    inByte = mySerial.read();
+
+  if(inByte == 'a'){
+    mySerial.println("Mode A");
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+    delay(200);
+    digitalWrite(3, LOW);
+    delay(200);
+  }  
+  else{
+    pinMode(3, INPUT);
+    digitalWrite(3, HIGH);
   int sensorValue1 = analogRead(1);
   int sensorValue2 = analogRead(2);
   int sensorValue3 = analogRead(3);
   mySerial.print(sensorValue1);
-  mySerial.print(", ");
+  mySerial.print(",");
   mySerial.print(sensorValue2);
-  mySerial.print(", ");
+  mySerial.print(",");
   mySerial.print(sensorValue3);
   mySerial.println();
+  }
+  
+  delay(10);
 } 
 
 
